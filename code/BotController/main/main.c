dc52a45da118e3c341330b0dc024c55d070a52d0
Jose Gutierrez del Arroyo <jose.gutierrezdelarroyo@afit.edu>
2024-10-02 21:32:36 -0400

    Implements Bluepad32 in ESP-IDF (no robot code)

---
+++ code/BotController/main/main.c
@@ -0,0 +1,47 @@
+// Example file - Public Domain
+// Need help? https://tinyurl.com/bluepad32-help
+
+#include <stdlib.h>
+
+#include <btstack_port_esp32.h>
+#include <btstack_run_loop.h>
+#include <btstack_stdio_esp32.h>
+#include <uni.h>
+
+#include "sdkconfig.h"
+
+// Sanity check
+#ifndef CONFIG_BLUEPAD32_PLATFORM_CUSTOM
+#error "Must use BLUEPAD32_PLATFORM_CUSTOM"
+#endif
+
+// Defined in my_platform.c
+struct uni_platform* get_my_platform(void);
+
+int app_main(void) {
+    // hci_dump_open(NULL, HCI_DUMP_STDOUT);
+
+    // Don't use BTstack buffered UART. It conflicts with the console.
+#ifdef CONFIG_ESP_CONSOLE_UART
+#ifndef CONFIG_BLUEPAD32_USB_CONSOLE_ENABLE
+    btstack_stdio_init();
+#endif  // CONFIG_BLUEPAD32_USB_CONSOLE_ENABLE
+#endif  // CONFIG_ESP_CONSOLE_UART
+
+    printf("Initializing program, waiting for BT connection...\n");
+    // Configure BTstack for ESP32 VHCI Controller
+    btstack_init();
+
+    // hci_dump_init(hci_dump_embedded_stdout_get_instance());
+
+    // Must be called before uni_init()
+    uni_platform_set_custom(get_my_platform());
+
+    // Init Bluepad32.
+    uni_init(0 /* argc */, NULL /* argv */);
+
+    // Does not return.
+    btstack_run_loop_execute();
+
+    return 0;
+}
